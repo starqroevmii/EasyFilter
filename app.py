608046c8bb641e7bc7e7e37c94b9bd879a0b1f35
starqroevmii <ptrishacarias@gmail.com>
2026-07-28 16:25:59 +0800

    streamlit no more

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import streamlit.components.v1 as components
-
-st.set_page_config(layout="wide")
-
-with open("index.html", "r", encoding="utf-8") as f:
-    html_code = f.read()
-
-components.html(html_code, height=900, scrolling=True)
